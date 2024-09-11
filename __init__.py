62f0c79b58917e044b58a339eb1f7c58dd076485
Eliashnf <90866455+Eliashnf@users.noreply.github.com>
2024-09-11 00:51:58 -0700

    Update __init__.py
    Delete bag gate way Histogramme

--- __init__.py
+++ __init__.py
@@ -31,9 +31,7 @@ def meteo():
 def mongraphique():
     return render_template("graphique.html")
 
-@app.route("/histogramme/")
-def mongraphique():
-    return render_template("histogramme.html")
+
 
 
 if __name__ == "__main__":
