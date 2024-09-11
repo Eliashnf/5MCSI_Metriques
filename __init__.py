32ce111860e3a528d3c138f49c0958049b62986c
Eliashnf <90866455+Eliashnf@users.noreply.github.com>
2024-09-11 00:59:07 -0700

    Update __init__.py
    add histogramme

--- __init__.py
+++ __init__.py
@@ -31,6 +31,9 @@ def meteo():
 def mongraphique():
     return render_template("graphique.html")
 
+@app.route("/histogramme/")
+def mongraphique():
+    return render_template("histogramme.html")
 
 
 
